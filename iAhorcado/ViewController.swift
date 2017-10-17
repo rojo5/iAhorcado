919b6e8a9e63717b532dc97d9afacdf6fbc1622d
CETYS <cetys@cetyss-mac.local>
2017-10-17 10:32:51 +0200

    Tine error estilo guadiana

--- iAhorcado/ViewController.swift
+++ iAhorcado/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Declaracion de variables	
+    
+    @IBOutlet weak var letrero: UILabel!
+    @IBOutlet weak var pantalla: UIImageView!
+    
+    var palabraOculta : String = "oculto"
+    var numeroFallos : Int = 0
+    let listaPalabras = ["YAVIN", "FELUCIA", "CORUSCANT", "KAMINO", "TATOOINE", "SCARIF", "BESPIN", "NABOO", "JEDHA"]
+    
+    
+    @IBAction func letras(_ sender: UIButton) {
+        
+    }
+    
+    func eligePalabraAAdivinar(){
+        
+        let posicion = Int(arc4random_uniform(UInt32(listaPalabras.count)))
+        
+        palabraOculta = listaPalabras[posicion]
+        
+    }
+    
+    func ponerGuiones(){
+        var guiones: String = ""
+        for i in 0 < palabraOculta.characters.count{
+            guiones += guiones + "_"
+        }
+        letrero.text = guiones
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        eligePalabraAAdivinar()
+        ponerGuiones()
     }
 
     override func didReceiveMemoryWarning() {
